7d669f6eb7331c7e2bf9de0d96b33ab579922df0
Nilo Marcos <nilo.n2m15@gmail.com>
2021-10-07 14:37:21 -0300

    implement of component Tasks

---
+++ src/components/Tasks/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Tasks() {
+  return(
+    <View>
+      <Text>This is a Task</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+  },
+});
